b981a1ea715043bcad30b20b2927963b10693faa
alishakor <aliuokeowo@gmail.com>
2023-08-04 03:14:27 +0100

    adjusting data types

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -6,12 +6,13 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[int, str]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
         a type-annotated function sum_mixed_list which
         takes a list mxd_lst of integers and floats and
         returns their sum as a float
     """
+    
     result = 0
     for element in mxd_lst:
         result += element
--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -6,7 +6,7 @@
 from typing import Callable
 
 
-def make_multiplier(multiplier: str) -> Callable[[float], float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """ a type-annotated function make_multiplier that takes
         a float multiplier as argument and returns a function
         that multiplies a float by multiplier
