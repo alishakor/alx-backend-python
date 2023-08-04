5773e1967bfb903fada95f73968976d254fc168c
alishakor <aliuokeowo@gmail.com>
2023-08-04 03:34:19 +0100

    modifying the return list

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -6,7 +6,7 @@
 from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: Iterable[Sequence]) -> list[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """a typed-function that returns iterables
     """
     return [(i, len(i)) for i in lst]
