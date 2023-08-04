ef3d1e5ab222606952c730ab1178713e625aea2e
alishakor <aliuokeowo@gmail.com>
2023-08-04 02:14:48 +0100

    script that returns a mixed list of int and float numbers

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    a script that returns the sum of a mixed
+    list of integer and floating numbers
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, str]]) -> float:
+    """
+        a type-annotated function sum_mixed_list which
+        takes a list mxd_lst of integers and floats and
+        returns their sum as a float
+    """
+    result = 0
+    for element in mxd_lst:
+        result += element
+    return result
